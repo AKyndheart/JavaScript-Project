ace2b9176e5d7d882fda90995f5f13b7696749f1
Andrew <klyzub102991@aol.com>
2025-03-25 10:16:26 -0400

    Display 6

--- index.js
+++ index.js
@@ -2,25 +2,29 @@
 // Main API: "http://www.omdbapi.com/?apikey=c5dce6dd&"
 // Poster API: "http://img.omdbapi.com/?apikey=c5dce6dd&"
 
+
+
 async function movieSearch() {
     const movies = await fetch("http://www.omdbapi.com/?apikey=c5dce6dd&s=fast")
     const moviesData = await movies.json()
     const borderEl = document.querySelector('.border')
     console.log(moviesData.Search)
+    borderEl.innerHTML = moviesData.Search.map((movie) => topMovies(movie)).join("")}
 
-    borderEl.innerHTML = moviesData.Search.map(
-            (movie) => `<div class="border">
-                            <figure class="landing__figure">
-                                <img src="${movie.Poster}" alt="" class="landing__img">
-                            </figure>
+movieSearch()
+
+    for (let i = 0; i < 6; ++i) {
+        function topMovies (movie) {
+            return`<div class="border">
+                        <figure class="landing__figure">
+                        <img src="${movie.Poster}" alt="" class="landing__img">
+                        </figure>
                             <h2 class="landing__title">
                                 ${movie.Title}
                             </h2>
                             <h2 class="landing__year">
                                 ${movie.Year}
                             </h2>
-                        </div>`
-            ).join("")
+                    </div>`
         }
-
-movieSearch()
+    }
