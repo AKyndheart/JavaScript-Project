6321bd7bda446cbd598f626b2e5ac8fe16c1820c
Andrew <klyzub102991@aol.com>
2025-04-05 21:04:58 -0400

    funcioning search bar

--- index.js
+++ index.js
@@ -8,24 +8,32 @@ async function movieSearch() {
     const movies = await fetch("https://www.omdbapi.com/?apikey=c5dce6dd&s=fast")
     const moviesData = await movies.json()
     const borderEl = document.querySelector('.landing__border')
-    console.log(moviesData.Search)
+    // console.log(moviesData.Search)
     borderEl.innerHTML = moviesData.Search.slice(0 , 6).map((movie) => topMovies(movie)).join("")
 }
 
 movieSearch()
 
-    for (let i = 0; i < 6; ++i) {
         function topMovies (movie) {
             return`<div class="border">
                         <figure class="landing__figure">
                         <img src="${movie.Poster}" alt="" class="landing__img">
-                        </figure>
                             <h2 class="landing__title">
                                 ${movie.Title}
                             </h2>
                             <h2 class="landing__year">
                                 ${movie.Year}
                             </h2>
+                        </figure>
                     </div>`
         }
-    }
+
+        async function onSearchChange(event) {
+            const id = (event.target.value)
+            const movies = await fetch(`https://www.omdbapi.com/?apikey=c5dce6dd&s=${id}`)
+            const moviesData = await movies.json()
+            const borderEl = document.querySelector('.landing__border')
+            borderEl.innerHTML = moviesData.Search.slice(0 , 6).map((movie) => topMovies(movie)).join("")
+        }
+
+        onSearchChange(event)
